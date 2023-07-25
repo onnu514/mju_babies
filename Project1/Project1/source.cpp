0eb4b15d60ad5029ca3ab33650bc0d3f3c1310c3
Kim Daehyeon <daehyeon3351@gmail.com>
2023-07-26 00:21:42 +0900

    첫 번째 과제

--- Project1/Project1/source.cpp
+++ Project1/Project1/source.cpp
@@ -3,12 +3,7 @@
 #include<string.h>
 #include<stdlib.h>
 
-//자료형? -> 어떤 데이터를 저장할 수 있는 형태
-//int -> 정수 데이터를 저장할 수 있는 형태
-//새로운 자료형 -> 학생 자료형을 만들어보자
 struct student {
-	//속성들
-	//이름 나이 학번
 	char* name;
 	int age;
 	char* id;
@@ -32,26 +27,21 @@ void printStu(struct student* stu) {
 
 
 int main() {
-	//숫자를 입력받아서 해당 수 만큼 struct student*를 만드는 경우
-	struct student** students;// == struct student*의 배열
+	//학생을 관리하는 프로그램
+	//모든 기능은 함수로
+	//1을 누르면 학생 추가(모든 정보를 입력받아 추가)
+	//2를 누르면 학생 삭제(id를 입력받아 비교)
+	//3을 누르면 특정 학생 조회(id를 입력받아 비교)
+	//4를 누르면 모든 학생 조회
+	//5를 누르면 모든 학생을 나이 순으로 정렬
+	//6을 누르면 종료 
+	struct student** students;
 	int num;
 	scanf("%d", &num);
 	students = (struct student**)malloc(sizeof(struct student*) * num);
-	//현재 상황
-	//배열은 만들어짐
-	//근데 배열 요소들(struct student* 들)은 초기화가 되지 않음
-	//students[0]->age = 4;//오류가 발생할 것임
-	//이유는 동적 할당을 아직 안 해서
-	//배열을 만들었음
-	//struct student*들이 num개 만큼 있죠?
-	//struct student*는 뭐냐 struct student의 주소를 가리킴
-	//쓸려면 동적 할당을 해야 한다
 	for (int i = 0; i < num; i++) {
 		students[i] = (struct student*)malloc(sizeof(struct student));
 	}
-
-	//배열도 만들었고, 모든 요소들도 동적할당(초기화) 완료.
-	//이제 값을 넣을 준비가 됨
 	for (int i = 0; i < num; i++) {
 		inputStu(students[i]);
 	}
